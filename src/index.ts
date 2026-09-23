a2fc8f02be4e81a373d86d49cf2e65d600c6a664
Valeria-Akshina <aksinalera@gmail.com>
2026-09-23 15:20:22 +0300

    ex one

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+// Задание 2.1
+let studentName: string = "Анна";
+let studentAge: number = 20;
+let isEnrolled: boolean = true;
+let middleName: null = null;
+let hobby: undefined = undefined;
+
+// Задание 2.2
+let city = "Москва";
+let population = 12_000_000;
+let isCapital = true;
+
+// Задание 2.3
+let title: string = "Книга";
+title = "Число";
+
+let pages: number = 300;
+pages = 300;
+
+let isRead: boolean = false;
+isRead = true;
+
+// Задание 2.4
+// let userName: string = null; 
+
+let userName1: string | null = null;
+let userName2: string = "";
+
+
+
